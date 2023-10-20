4135f6c2eed963b65a8bde056235583b6b91d5e2
GlazunovVLAD <klad.yandex.ru@gmail.com>
2023-10-20 20:15:39 +0300

    v3_final

--- Гуасса.py
+++ Гуасса.py
@@ -5,7 +5,7 @@ test1 = [[1, 2, -1, -1, 0],
          [2, 5, 2, 1, 3],
          [3, 5, 1, 2, 5]]
 
-test2 = [[0, 1, 1, 2, 2],
+test2 = [[4, 1, 1, 2, 2],
          [1, 3, 2, -1, 2],
          [2, -1, 5, 3, -1],
          [4, 5, 4, -4, 8]]
@@ -15,29 +15,32 @@ print(hod, end='\n\n')
 
 
 def Th_Gauss():  # сделать так, чтобы при нуле в начале ничего не ломалось
-    for row_number in range(0, len(hod)):
+    n = len(hod)
+    for row_number in range(0, n):
         if hod[row_number][row_number] == 0:
-            hod[row_number], hod[len(hod)] = hod[len(hod)], hod[row_number]
-        for next_row_number in range(row_number + 1, len(hod)):
+            row_to_exchange = -10
+            for a_a in range(row_number + 1, n):
+                if hod[a_a, row_number] != 0:
+                    row_to_exchange = a_a
+                    break
+            if row_to_exchange == -10:
+                raise Exception("слишком много решений")
+            hod[row_number], hod[row_to_exchange] = hod[row_to_exchange], hod[row_number].copy()
+        for next_row_number in range(row_number + 1, n):
             k = hod[next_row_number][row_number] / hod[row_number][row_number]
-            for column_number in range(row_number, len(hod) + 1):
+            for column_number in range(row_number, n + 1):
                 hod[next_row_number][column_number] = hod[next_row_number][column_number] - k * hod[row_number][column_number]
     print(hod)
 
-    xs = [0] * len(hod)
-    for x_number in range(len(hod) - 1, -1, -1):
+    xs = [0] * n
+    for x_number in range(n - 1, -1, -1):
         summa = 0
-        for previous_x_number in range(len(hod) - 1, x_number, -1):
+        for previous_x_number in range(n - 1, x_number, -1):
             summa += xs[previous_x_number] * hod[x_number][previous_x_number]
-        xs[x_number] = (hod[x_number][len(hod)] - summa) / hod[x_number][x_number]
+        xs[x_number] = (hod[x_number][n] - summa) / hod[x_number][x_number]
         print(x_number, xs[x_number])
     return xs
-    #
-    # x3 = hod[3][4] / hod[3][3]
-    # x2 = (hod[2][4] - hod[2][3] * x3) / hod[2][2]
-    # x1 = (hod[1][4] - hod[1][2] * x2 - hod[1][3] * x3) / hod[1][1]
-    # x0 = (hod[0][4] - hod[0][1] * x1 - hod[0][2] * x2 - hod[0][3] * x3) / hod[0][0]
-    # return [x1, x2, x3, x4]
+
 
 
 print(Th_Gauss())
